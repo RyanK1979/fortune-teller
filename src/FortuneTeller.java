ff1bcbef587d7bfd03e25da67faba1b737d9e3f1
RyanK79 <Nermal5@msn.com>
2018-01-22 09:24:31 -0500

    updated to final code

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -5,57 +5,109 @@ public class FortuneTeller {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Hello what is your First name?");
-		String firstName = input.nextLine();
+		// variables used
+		String firstName;
+		String lastName;
+		int age;
+		int month;
+		String favColor;
+		int siblings;
+		int yrsToRetirement;
+		String retireHome = null;
+		String retireCar = null;
+		double bankMoney;
+
+		// questioning sectioning
+
+		System.out.println("Hello, I'm going to attempt to tell you your future, what is your first name?");
+		firstName = input.nextLine();
 		System.out.println("What is your Last name?");
-		String lastName = input.nextLine();
+		lastName = input.nextLine();
 		System.out.println("How old are you?");
-		int age = input.nextInt();
+		age = input.nextInt();
 		input.nextLine();
 		System.out.println("What month were you born in 1 through 12?");
-		int month = input.nextInt();
+		month = input.nextInt();
 		input.nextLine();
 		System.out.println("What is your favorite ROYGBIV color? If you need help, type Help");
-		String FavColor = input.nextLine();
+		favColor = input.nextLine();
+		while (favColor.equalsIgnoreCase("help")) {
+			System.out.println("pick either red, orange, yellow, green, blue, indigo, or violet.");
+			favColor = input.nextLine();
+		}
 		System.out.println("How many siblings do you have?");
-		int siblings = input.nextInt();
+		siblings = input.nextInt();
 
-		System.out.println("Hello " + firstName + lastName + "");
+		if (siblings == 0) {
+			retireHome = "Tempe, AZ";
+		} else if (siblings == 1) {
+			retireHome = "San Diego, CA";
+		} else if (siblings == 2) {
+			retireHome = "Key West, FL";
+		} else if (siblings == 3) {
+			retireHome = "Nashville, TN";
+		} else if (siblings >= 4) {
+			retireHome = "Phoenix, AZ";
+		} else if (siblings < 0) {
+			retireHome = "Juarez, Mexico";
+		}
+
+		// to figure out retirement age
 		{
-			if (age % 2 ==0)	
-				System.out.println("will retire in 20 years");
-			else 
-				System.out.println("will retire in 23 years");
+			if (age % 2 == 0)
+				yrsToRetirement = 20;
+			else
+				yrsToRetirement = 23;
 		}
-		{	
-			if (month <= 4)	
-				System.out.println("you will have $379,450.12 in bank account");
-				else if (month <= 8)
-				System.out.println("you will have $421,163.19 in bank account");
-				else if (month <= 12)
-				System.out.println("you will have 3,415,321.73 in bank account");
-				else if (month >= 13)
-					System.out.println("you will be broke as a joke");
-			
+		// bank balance section
+		switch (month) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			bankMoney = 379450.12;
+			break;
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			bankMoney = 421163.19;
+			break;
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+			bankMoney = 3415321.73;
+			break;
+		default:
+			bankMoney = 0.00;
 		}
-		{
-			if (siblings == 0)
-				System.out.println("you will vacation in Tempe, AZ");
-			else if (siblings == 1)
-				System.out.println("you will vacation in San Diego, CA");
-			else if (siblings == 2)
-				System.out.println("you will vacation in Key West, FL");
-			else if (siblings == 3)
-				System.out.println("you will vacation in Nashville, TN");
-			else if (siblings >= 4)
-				System.out.println("you will vacation in Pheonix, AZ");
-			else if (siblings < 0)
-				System.out.println("you will vacation in Juarez, Mexico");
+		input.nextLine();
+
+		// last but not least, what kind of car do they get
+
+		if (favColor.equalsIgnoreCase("red")) {
+			retireCar = "Ford Mustang";
+		} else if (favColor.equalsIgnoreCase("orange")) {
+			retireCar = "Ford F150";
+		} else if (favColor.equalsIgnoreCase("yellow")) {
+			retireCar = "Volkswagen Bug";
+		} else if (favColor.equalsIgnoreCase("green")) {
+			retireCar = "Toyota Prius";
+		} else if (favColor.equalsIgnoreCase("blue")) {
+			retireCar = "Chevy Cruze";
+		} else if (favColor.equalsIgnoreCase("indigo")) {
+			retireCar = "Tesla Model S";
+		} else if (favColor.equalsIgnoreCase("violet")) {
+			retireCar = "Honda CRV";
 		}
-		if ( FavColor == "red")
-			System.out.println("you will drive a Mustang");
-			
-		
+
+		System.out.println(firstName.substring(0, 1).toUpperCase() + (firstName.substring(1).toLowerCase()) + " "
+				+ (lastName.substring(0, 1).toUpperCase()) + (lastName.substring(1).toLowerCase())
+				+ " you will retire in " + yrsToRetirement + " years with $" + bankMoney
+				+ " in the bank, a lovely estate in " + retireHome + " and drive a sweet " + retireCar + ".");
+		System.out.println("Thank you so much for giving me a purpose!");
+
 		input.close();
 	}
 
